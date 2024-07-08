87a20e156e6f0ed006fabe787f8db7bf4772dcb2
Yan Velasco <yancarlosduartevelasco@gmail.com>
2024-07-08 14:15:03 -0300

    Create a button to dowmload image

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -23,7 +23,7 @@ export class ImageCard extends React.Component<ImageCardProps> {
         return (
             <div className="mt-8 p-4 hover:cursor-pointer">
                 <h2 className="card relative bg-white-100 rounded-md transition-transform ease-in duration-500 hover:scale-105 transform hover:shadow-lg hover:translate-y-2">
-                    <img onClick={downloadImage} className="h-56 w-full object-cover rounded-t-md" alt={title}
+                    <img className="h-56 w-full object-cover rounded-t-md" alt={title}
                          src={imageUrl}/>
                     <div className="card-body p-4">
                         <h5 className="text-xl font-semibold mb-2 text-gray-600">
@@ -35,6 +35,10 @@ export class ImageCard extends React.Component<ImageCardProps> {
                         <p className="text-gray-600">
                             {dateUploaded}
                         </p>
+                        <button onClick={downloadImage}
+                                className="absolute bottom-0 right-0 bg-blue-500 text-white px-4 py-2 rounded-md text-sm m-4 hover:bg-blue-700">
+                            Download
+                        </button>
                     </div>
                 </h2>
             </div>
