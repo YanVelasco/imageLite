7755dc5a7163ebd964fec62bcc115b6dda6b5812
Yan Velasco <yancarlosduartevelasco@gmail.com>
2024-08-10 17:43:46 -0300

    feet: localhost 3000

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+'use client'
+
+import { authService } from "@/resources/users/authentication.resource.service";
+import Login from "@/app/login/page";
+import Gallery from "@/app/gallery/page";
+
 export default function Home() {
+
+    const user = authService.getUserSessionToken();
+
+    if(user){
+        return <Gallery />
+    }
+
   return (
-    <main >
-      <h1>Home</h1>
-    </main>
+    <Login />
   )
 }
