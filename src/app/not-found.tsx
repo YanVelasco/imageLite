cecb6600476cf643272b4718c6884491b47ff0c1
Yan Velasco <yancarlosduartevelasco@gmail.com>
2024-08-11 11:57:47 -0300

    feet: page not found created

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function NotFound() {
+    return (
+            <section className="flex flex-col items-center justify-center h-screen text-red-900 text-xl font-bold text-center">
+                <h1>404 - Page Not Found</h1>
+            </section>
+    )
+}
